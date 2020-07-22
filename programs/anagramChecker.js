351e2faa72d8c3dbcdfefa8a3a5aa59f43e082f0
Michael <mike.leibbrandt@gmail.com>
2020-07-22 21:27:29 +0200

    Improved anagramChecker

--- programs/anagramChecker.js
+++ programs/anagramChecker.js
@@ -4,32 +4,28 @@
  */
 
 const checkAnagram = (word,word1) => {
-	if (word.length !== word1.length) {
-		return false
-	}
-	const freqWord = {}	
-	const freqWord1 = {}	
+    if (word.length !== word1.length) {
+        return false
+    }
+    const freqWord = {} 
 
-	for (const element of word){
-		freqWord[element] = (freqWord[element] || 0) + 1
-	}
+    for (const element of word){
+        freqWord[element] = (freqWord[element] || 0) + 1
+    }
 
-	for (const element of word1){
-		freqWord1[element] = (freqWord1[element] || 0) + 1
-	}
 
-	for(const key in freqWord){
-		if (!(key in freqWord1)){
-			return false
-		}
-
-		if(freqWord1[key] !== freqWord[key]){
-			return false
-		}
-	}
-	return true
+    for(let i =0; i < word1.length; i++ ){
+        let letter = word1[i]
+        if (!(freqWord[letter])){
+            return false
+        } else {
+            if (freqWord[letter]){
+                freqWord[letter] -= 1
+            }
+        }
+    }
+    return true
 }
-
 
 console.log(checkAnagram("",""))
 console.log(checkAnagram("aaz","zza"))
